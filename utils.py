9d3eafe8f937f5808faac2df550dbe3b6a5a753c
faraz-m-awan <faraz.awan0406@gmail.com>
2024-10-26 13:47:05 +0100

    First Commit

---
+++ utils.py
@@ -0,0 +1,31 @@
+
+######################################################
+
+#           Passanger Car Equilence                  #
+
+######################################################
+
+PEDAL_CYCLES = 0.2  # Typical range: 0.2 – 0.5
+TWO_WHEELED_MOTOR_VEHICLES = 0.6  # Typical range: 0.5 – 0.8
+CARS_AND_TAXIS = 1.0
+BUSES_AND_COACHES = 2.5  # Typical range: 2.0 – 3.0
+LGVS = 1.2  # Light Goods Vehicles (Typical range: 1.0 – 1.5)
+HGVS_2_RIGID_AXLE = 1.8  # Typical range: 1.5 – 2.0
+HGVS_3_RIGID_AXLE = 2.3  # Typical range: 2.0 – 2.5
+HGVS_4_OR_MORE_RIGID_AXLE = 2.8  # Typical range: 2.5 – 3.0
+HGVS_3_OR_4_ARTICULATED_AXLE = 3.0  # Typical range: 2.5 – 3.5
+HGVS_5_ARTICULATED_AXLE = 3.7  # Typical range: 3.5 – 4.0
+HGVS_6_ARTICULATED_AXLE = 4.2  # Typical range: 4.0 – 4.5
+
+
+
+######################################################
+
+#           Road Capacity cars/hour                  #
+
+######################################################
+
+AVG_ROAD_CAPACITY_A=1750 # Typical range: 1,500–2,000 vehicles/hour"
+AVG_ROAD_CAPACITY_U=1150 # Typical range: 800–1,500 vehicles/hour"
+AVG_ROAD_CAPACITY_C=500  # Typical range: 200–800 vehicles/hour"
+AVG_ROAD_CAPACITY_B=500  # Typical range: 200–800 vehicles/hour"
